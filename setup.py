d2f6c29ac18073376da5dd2d8a9c95dd09f250bf
Alex Chan <alex@twilio.com>
2016-07-28 01:04:12 -0500

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+	# Basic package information.
+	name = 'slack',
+	version = '0.0.0',
+	packages = ['slack'],
+	include_package_data = True,
+	install_requires = ['httplib2', 'simplejson'],
+	license='LICENSE.txt',
+	url = 'https://github.com/alexcchan/slack/tree/master',
+	keywords = 'slack api',
+	description = 'Slack API v2 Wrapper for Python',
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Topic :: Internet'
+	],
+)
+
+
